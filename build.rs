3623ec5a407f0086cd783f5066684e6fc8974149
Sergio Gasquez <sergio.gasquez@gmail.com>
2024-01-11 17:11:57 +0100

    style: Fix rustfmt errors

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("cargo:rustc-link-arg-bins=-Tlinkall.x");
-    {% if wifi %}
+    {% if wifi -%}
     println!("cargo:rustc-link-arg-bins=-Trom_functions.x");
-    {% endif -%}
+    {% endif %}
 }
